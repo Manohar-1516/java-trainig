76d505d2e44862de9e81e7b4498141705baee710
Manohar-1516 <durgamanohat@gmail.com>
2025-01-06 10:08:03 +0530

    Firstcommit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x =5;
+        int y =7;
+        
+        if(x>y){
+            System.out.println("X is greater");
+        }else{
+            System.out.println("Y is greater");
+        }
+    }
+}

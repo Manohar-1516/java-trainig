2f9216d47b80fbe1bf9d92feb415f476a59451f0
Manohar-1516 <durgamanohat@gmail.com>
2025-01-08 09:17:25 +0530

    Second Commit

---
+++ src/Day_5_Loops.java
@@ -0,0 +1,22 @@
+
+
+public class Day_5_Loops {
+    public static void main(String[] args) {
+        String c ="ClassRoom";
+
+        System.out.println(c.charAt(5));
+        System.out.println(c.length());
+        System.out.println(c.substring(3,7));
+        System.out.println(c.substring(0));
+        System.out.println("hello".equals("hello"));
+        System.out.println("hello"+2+2);
+
+        // for loop
+        for(int i=0;i<10;i++){
+            System.out.println("Manu");
+        }
+
+
+    }
+
+}
